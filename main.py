052646496628d5cc7cd29821d4d0e90f04179e75
Tsing <2719584131@qq.com>
2024-12-31 14:52:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ import warnings
 
 warnings.simplefilter("ignore")
 
-# ensure result consistency
-seed = 42
-torch.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-np.random.seed(seed)
-random.seed(seed)
 
 from algo.server.Real import Server as Real
 from algo.server.Gen import Server as Gen
