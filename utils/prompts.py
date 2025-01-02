8e880da61daa8243fe1c94077f6da7faa25924ca
Tsing <2719584131@qq.com>
2025-01-02 13:44:47 +0800

    Update prompts.py

--- utils/prompts.py
+++ utils/prompts.py
@@ -2,8 +2,8 @@
 # exemple_prompt = "{EXEMPLE}"
 # negative_prompt = None
 
-base_prompt = "A clear {DOMAIN} image of {LABEL}"
-exemple_prompt = "A clear {DOMAIN} image of {LABEL}, {EXAMPLE}"
+base_prompt = "A clear {DOMAIN} image with {LABEL}"
+exemple_prompt = "A clear {DOMAIN} image with {LABEL}, {EXAMPLE}"
 negative_prompt = None
 
 refine_prompt = "refine this description of images to introduce rich context: "
